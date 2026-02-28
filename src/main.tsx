879f0a18d9f840e98a6178fe2331f52ea42c8142
Hruizzd <ruizdelgadohugo1004@gmail.com>
2026-02-28 13:58:10 +0100

    analitycs

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './styles/index.css';
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics';
+
+inject();
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
